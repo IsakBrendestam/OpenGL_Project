ed84ec5ccac6096dc23dc85abb4099f320918e01
IsakBrendestam <isak.kruger01@gmail.com>
2024-10-30 13:13:57 +0100

    Same Triangle - less code

--- main.cpp
+++ main.cpp
@@ -24,31 +24,19 @@ void Render(unsigned int shaderProgram)
 {
     unsigned int VBO;
     glGenBuffers(1, &VBO);  
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);  
-
-    glUseProgram(shaderProgram);
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);  
-
     glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);  
 
-    glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    
+
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
